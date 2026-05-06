13286e7b972a8646c3a62d55f78f9bd4610da47b
aurusxd <warface.clan01@gmail.com>
2026-05-06 15:13:32 +0700

    Настроен EF, добавлены миграции (closes #2)

---
+++ DeliveryService/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DeliveryService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Courier> Couriers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<RoutePoint> RoutePoints { get; set; }
+        public DbSet<OrderStatusHistory> OrderStatusHistories { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+
+}
