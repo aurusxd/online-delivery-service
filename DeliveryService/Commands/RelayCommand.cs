10658d47de7ecbcb1353a0d8136f27f551c96407
Entity <kdanyl3103@gmail.com>
2026-05-06 17:29:30 +0700

    Добавление папки для RelayCommand, а также 2 вида RelayCommand (closes #4)

---
+++ DeliveryService/Commands/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Input;
+
+namespace DeliveryService.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool>? _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        public RelayCommand(Action<object> execute, Func<object, bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+                return;
+
+            _execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+}
---
+++ DeliveryService/Commands/RelayCommandAsync.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DeliveryService.Commands
+{
+    public class RelayCommandAsync : ICommand
+    {
+        private readonly Func<object, Task> _execute;
+        private readonly Func<object, bool> _canExecute;
+        private bool _isExecuting;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        public RelayCommandAsync(Func<object, Task> execute, Func<object, bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? (_ => true);
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            return !_isExecuting && _canExecute(parameter);
+        }
+
+        public async void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+                return;
+
+            _isExecuting = true;
+            CommandManager.InvalidateRequerySuggested();
+
+            try
+            {
+                await _execute(parameter);
+            }
+            finally
+            {
+                _isExecuting = false;
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+}
