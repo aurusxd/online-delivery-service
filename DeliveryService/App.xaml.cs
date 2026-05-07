97915553decb13ef9ecbe80efcae00440e18420b
aurusxd <warface.clan01@gmail.com>
2026-05-07 12:36:49 +0700

    Внедрил базовый DI

--- DeliveryService/App.xaml.cs
+++ DeliveryService/App.xaml.cs
@@ -13,19 +13,37 @@ namespace DeliveryService
     /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider? Services { get; private set; }
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            var services = new ServiceCollection();
-
+            
             var config = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json")
             .Build();
 
 
+            var services = new ServiceCollection();
+
+
+            // БД
             services.AddDbContext<AppDbContext>(options =>
                 options.UseNpgsql(config.GetConnectionString("Default")));
+
+
+            // Репозитории
+
+            // Сервисы
+
+            // ViewModels
+
+            // Собираем контейнер
+            Services = services.BuildServiceProvider();
+
+            // Открываем главное окно
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
         }
     }
 
