601555631ddd75f2d70c4bed13bb3122700636a4
Entity <kdanyl3103@gmail.com>
2026-05-07 15:13:22 +0700

    Добавление CourierRepository для взаимодействия с Курьерами в бд (closes #9)

---
+++ DeliveryService/Repositories/CourierRepository.cs
@@ -0,0 +1,96 @@
+﻿using DeliveryService.Data;
+using DeliveryService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryService.Repositories
+{
+    /// <summary>
+    /// Репозиторий для доступа к Курьерам в базе данных
+    /// </summary>
+    public class CourierRepository
+    {
+        private readonly AppDbContext _context;
+
+
+        public CourierRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        /// <summary>
+        /// Получение курьера по id
+        /// </summary>
+        /// <param name="courierId">ID курьера</param>
+        /// <returns>Курьер</returns>
+        public async Task<Courier?> GetById(int courierId) => await _context.Couriers.FindAsync(courierId);
+
+        /// <summary>
+        /// Получение всех курьеров
+        /// </summary>
+        /// <returns>Список курьеров</returns>
+        public async Task<List<Courier>> GetAllAsync()
+        {
+            return await _context.Couriers
+                .Include(o => o.Orders)
+                .ToListAsync();
+        }
+
+        /// <summary>
+        /// Получение всех активных курьеров
+        /// </summary>
+        /// <returns>Список курьеров, у которых IsActive равен True</returns>
+        public async Task<List<Courier>> GetActive()
+        {
+            return await _context.Couriers
+                .Where(c => c.IsActive)
+                .Include(o => o.Orders)
+                .ToListAsync();
+        }
+
+        /// <summary>
+        /// Добавление курьера
+        /// </summary>
+        /// <param name="courier">Курьер</param>
+        public async Task AddAsync(Courier courier)
+        {
+            await _context.Couriers.AddAsync(courier);
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Обновление курьера в базе данных
+        /// </summary>
+        /// <param name="courier">Курьер</param>
+        public async Task UpdateAsync(Courier courier)
+        {
+            _context.Couriers.Update(courier);
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Переключение статуса IsActive 
+        /// </summary>
+        /// <param name="courierId">ID курьера</param>
+        public async Task ToggleOnline(int courierId)
+        {
+            var courier = await _context.Couriers.FindAsync(courierId);
+
+            if (courier != null)
+            {
+                courier.IsActive = !courier.IsActive;
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        /// <summary>
+        /// Удаление курьера
+        /// </summary>
+        /// <param name="courier">Курьер</param>
+        public async Task DeleteAsync(Courier courier)
+        {
+            _context.Couriers.Remove(courier);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
