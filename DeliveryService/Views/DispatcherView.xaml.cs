e6671e8ee23b6790698063449f14af1e0f9c67ab
aurusxd <warface.clan01@gmail.com>
2026-05-08 13:58:23 +0700

    fix resolve conflict

--- DeliveryService/Views/DispatcherView.xaml.cs
+++ DeliveryService/Views/DispatcherView.xaml.cs
@@ -1,23 +1,6 @@
-<<<<<<< HEAD
 ﻿using DeliveryService.Utils;
-using GMap.NET;
-=======
-﻿using GMap.NET;
->>>>>>> 8878fab27af73cdd7eca8b6682f01abf3c572c0a
-using GMap.NET.MapProviders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace DeliveryService.Views
 {
