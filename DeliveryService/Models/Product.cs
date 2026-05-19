b5a32565df046cdbf80aa074a7a70ea6f5b7bf71
Sasha <sasha2911666@gmail.com>
2026-05-19 18:37:36 +0700

    удаление хардкода, переход на ListView, добавление стиля категорий

---
+++ DeliveryService/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace DeliveryService.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Weight { get; set; }
+        public string Price { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
--- DeliveryService/Views/MenuView.xaml.cs
+++ DeliveryService/Views/MenuView.xaml.cs
@@ -11,17 +11,33 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using DeliveryService.Models;
 
 namespace DeliveryService.Views
 {
     /// <summary>
     /// Логика взаимодействия для MenuView.xaml
     /// </summary>
-    public partial class MenuView : Window
-    {
-        public MenuView()
+   
+        public partial class MenuView : Window
         {
-            InitializeComponent();
+           
+            public ObservableCollection<Product> MenuItems { get; set; }
+
+            public MenuView()
+            {
+                InitializeComponent();
+
+                MenuItems = new ObservableCollection<Product>
+            {
+                new Product { Name = "Плов с курицей", Weight = "250 г", Price = "249 руб", ImagePath = "/DeliveryService;component/Images/plov.png" },
+                new Product { Name = "Круассан", Weight = "60 г", Price = "89 руб", ImagePath = "/DeliveryService;component/Images/croissant.png" },
+                new Product { Name = "Капучино", Weight = "300 мл", Price = "149 руб", ImagePath = "/DeliveryService;component/Images/coffee.png" },
+                new Product { Name = "Чизкейк", Weight = "100 г", Price = "179 руб", ImagePath = "/DeliveryService;component/Images/cheesecake.png" }
+            };
+
+                DataContext = this;
+            }
         }
     }
-}
