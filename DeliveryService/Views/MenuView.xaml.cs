8e03312fda399d233e4584fa36c74e8bb2747271
aurusxd <warface.clan01@gmail.com>
2026-05-20 09:29:23 +0700

    убрал лишнее

--- DeliveryService/Views/MenuView.xaml.cs
+++ DeliveryService/Views/MenuView.xaml.cs
@@ -23,21 +23,9 @@ namespace DeliveryService.Views
         public partial class MenuView : Window
         {
 
-        public ObservableCollection<Product> MenuItems { get; set; }
-
         public MenuView()
         {
             InitializeComponent();
-
-            MenuItems = new ObservableCollection<Product>
-            {
-                new Product { Name = "Плов с курицей", Weight = "250 г", Price = "249 руб", ImagePath = "/DeliveryService;component/Images/plov.png" },
-                new Product { Name = "Круассан", Weight = "60 г", Price = "89 руб", ImagePath = "/DeliveryService;component/Images/croissant.png" },
-                new Product { Name = "Капучино", Weight = "300 мл", Price = "149 руб", ImagePath = "/DeliveryService;component/Images/coffee.png" },
-                new Product { Name = "Чизкейк", Weight = "100 г", Price = "179 руб", ImagePath = "/DeliveryService;component/Images/cheesecake.png" }
-            };
-
-            DataContext = this;
         }
 
         private void ListView_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
